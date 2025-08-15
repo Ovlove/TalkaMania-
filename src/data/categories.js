74e6735a1bd127601cc7a4f706bcd65cb6a627c3
Ovlove <ovwemue@gmail.com>
2025-08-15 12:14:27 +0100

    Create categories.js

---
+++ src/data/categories.js
@@ -0,0 +1,14 @@
+const categories = [
+  { name: "Home", slug: "home" },
+  { name: "Fashion", slug: "fashion" },
+  { name: "Food", slug: "food" },
+  { name: "Entertainment", slug: "entertainment" },
+  { name: "Sports", slug: "sports" },
+  { name: "Wrestling", slug: "wrestling" },
+  { name: "Fiction", slug: "fiction" },
+  { name: "Kids", slug: "kids" },
+  { name: "Movies", slug: "movies" },
+  { name: "K-Drama", slug: "kdrama" }
+];
+
+export default categories;
