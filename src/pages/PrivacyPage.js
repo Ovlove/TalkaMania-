c5ccebfc198c09f5def4d9b5406f94b8fc76f73d
Ovlove <ovwemue@gmail.com>
2025-08-15 11:57:32 +0100

    Create PrivacyPage.js

---
+++ src/pages/PrivacyPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PrivacyPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
+      <p>
+        TalkaMania respects your privacy. We only collect necessary data to provide a better experience, such as email addresses for newsletter subscriptions. We do not share personal information with third parties without your consent.
+      </p>
+      <p className="mt-4">
+        By using our website, you agree to our privacy practices.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPage;
